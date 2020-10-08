2bdd8cc5827fab4010266880a738a7f2bb229eb0
Nanaika <zloyx2@mail.ru>
2020-10-08 17:45:38 +0500

    ------

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,34 +1,32 @@
 package com.company;
 
 
+import java.io.File;
+
 public class Main {
 
 
-    public static void main(String[] args)  {
+    public static void main(String[] args) {
 
 
-
-
+        File file = new File(args[3]);
 
 
         FiledataRecord fileDataRecord = new FiledataRecord(args[0]);
 
 
+        fileDataRecord.recordLogin();
+        fileDataRecord.recordPass();
+        fileDataRecord.recordPassFiles();
 
-
-            fileDataRecord.recordLogin();
-            fileDataRecord.recordPass();
-
-            fileDataRecord.recordPassFiles();
-
-        DirParser dirParser = new DirParser(args[1],".txt");
+        DirParser dirParser = new DirParser(file.getAbsolutePath(), ".txt");
         dirParser.printFiles();
 //        System.out.println(dirParser.getCurrentDir());
-//        dirParser.setNewDir("C:\\AMD\\");
 //        System.out.println(dirParser.getCurrentDir());
 //        dirParser.setNewDir(args[1]);
 //        dirParser.setFilter(".jpg");
 //        dirParser.printFiles();
+
     }
 
 
